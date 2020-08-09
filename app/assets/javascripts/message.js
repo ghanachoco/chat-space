831b585387f9a8cceb75d1b875e22267b942e782
河村侑 <ghanachoco.09@gmail.com>
2020-08-09 13:57:32 +0900

    メッセージの非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="message">
+          <div class="info">
+            <div class="info__name">
+              ${message.user_name}
+            </div>
+            <div class="info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="text">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message">
+          <div class="info">
+            <div class="info__name">
+              ${message.user_name}
+            </div>
+            <div class="info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="text">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    // console.log('hoge')
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.submit-btn').prop("disabled", false)
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  });
+});
